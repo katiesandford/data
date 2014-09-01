f8835f5b0146d31066a59eb6f62c5c3cd51390db
Katie <katie.l.sandford@gmail.co.uk>
2014-09-01 17:57:49 +0100

    tidy up task1.php

--- dataTest/scripts/task1.php
+++ dataTest/scripts/task1.php
@@ -6,15 +6,15 @@ require 'vendor/autoload.php';
 use Graze\Account;
 use Graze\Database;
 
-// Get all account ids from the live schema
 $reportingDb = Database::getConnection('reporting');
 $liveDb = Database::getConnection('live');
 $sql = 'SELECT id FROM account';
 
+// Get all account ids from the live schema
 $accountIds = $liveDb->fetchAllPrepared($sql);
 
 
-function parseThirdPartyFiles (){
+function parseThirdPartyFiles(){
 	$iterator = new DirectoryIterator('/home/vagrant/data/dataTest/data');
 	$thirdPartySignUpData = array();
 	//Iterate though files in the data directory
@@ -32,8 +32,6 @@ function parseThirdPartyFiles (){
 		  		if (array_key_exists($lineData[0], $thirdPartySignUpData){
 		  			throw new Exception("File *".$itemInfo->getFilename().") contains duplicate data: ".$lineData);
 		  		}
-		  		//$lineData[1] = date
-		  		//$lineData[2] = promotion code
 		  		$thirdPartySignUpData[$lineData[0]] = array($lineData[1], $lineData[2]);
 		  	}
 			
@@ -48,8 +46,6 @@ print_r($thirdPartySignUpData);
 function createReportingAccount($accountId){
 
 	$account = new Account($accountId);
-
-	//Get conveversion date
 	$firstFullPriceBox = $account->getFirstBoxId(300);
 	$conversionDate = $firstFullPriceBox;
 	$numberOfBoxesSent = $account->getNumberOfBoxesSent();
@@ -57,7 +53,8 @@ function createReportingAccount($accountId){
 	$firstChurnDate = $account->getFirstChurnDate();
 	$totalRevenue = $account->getTotalRevenue();
 	$userEnteredPromotionCode = $account->getPromotionCode();
-	$thirdPartySuppliedPromotionCode = $thirdPartySignUpData[$account->getEmailAddress()];
+	$thirdPartySuppliedPromotionCode = $thirdPartySignUpData[$account->getEmailAddress()][1];
+	$schedulesTried = $account->getSchedulesTried();
 	
 	if ($userEnteredPromotionCode AND $thirdPartySuppliedPromotionCode){
 		throw new Exception('Account with id '.strval($accountId).'should not have singed up with a promotion channel if one has been supplied by a third party');
@@ -65,8 +62,8 @@ function createReportingAccount($accountId){
 
 	$promotionChannel = $userEnteredPromotionCode ?: $thirdPartySuppliedPromotionCode ?: "direct";
 
-	$sqlInsert = 'INSERT INTO reporing_account (account_id, conversion_date, boxes_sent, full_price_boxes_sent, first_churn_date, total_revenue, promotion_channel) VALUES (?,?,?,?,?,?)';
-	$reportingDb->queryPrepared($sqlInsert, array($accountId, $conversionDate, $numberOfBoxesSent, $numberOfFullPriceBoxesSent, $firstChurnDate, $totalRevenue, $promotionChannel));
+	$sqlInsert = 'INSERT INTO reporing_account (account_id, conversion_date, boxes_sent, full_price_boxes_sent, first_churn_date, total_revenue, promotion_channel, schedules_tried) VALUES (?,?,?,?,?,?)';
+	$reportingDb->queryPrepared($sqlInsert, array($accountId, $conversionDate, $numberOfBoxesSent, $numberOfFullPriceBoxesSent, $firstChurnDate, $totalRevenue, $promotionChannel, $schedulesTried));
 	print "Inserted row for account id ".strval($accountId)." into table reporting.reporting_account\n";
 }
 
